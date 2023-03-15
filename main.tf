b2d891747477cecdb7e7d15eef7db918601da854
utkarshxperi <91527380+utkarshxperi@users.noreply.github.com>
2023-03-15 13:11:05 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "ap-south-1"
+  access_key = "AKIAVOANXWL6VOJQXWUY"
+  secret_key = "EdrV7SxNr+oZRSzqWPToC33L4cEEBpWWqNoK6JZz"
+}
+
+
+
+locals {
+  repo_name = "new-pass"
+}
+
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "git clone https://github.com/utkarshxperi/${local.repo_name}.git"
+  }
+}
+
+locals {
+  files = fileset("${path.module}/${local.repo_name}", "**/*")
+}
+
+resource "aws_s3_object" "example" {
+  for_each = local.files
+
+  bucket = "gitpushdev"
+  key = "${local.repo_name}/${each.value}"
+  source = "${path.module}/${local.repo_name}/${each.value}"
+}
